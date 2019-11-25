51b3ec795fb0f3e42818d9eef80296beb8bb6109
kotobuki13 <kotobukigit@yahoo.co.jp>
2019-11-25 15:22:05 +0900

    Create a post screen appearance

---
+++ public_html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>JUST BBS FOR FUN</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+
+  <h1>JUST BBS FOR FUN</h1>
+
+  <form action="POST">
+      <input type="text" name="u_name" placeholder="名前（１０文字以内）" size="20">
+      <textarea name="content" placeholder="投稿内容(１５０文字以内)" cols="40" rows="5"></textarea>
+      <div class="btn">投稿する</div>
+  </form>
+
+</body>
+
+</html>
