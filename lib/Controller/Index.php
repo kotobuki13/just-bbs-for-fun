3ac1b836a5ecd09998edb41b3ca061ed039c8a53
kotobuki13 <kotobukigit@yahoo.co.jp>
2019-11-26 22:06:43 +0900

    create Index class and post method

---
+++ lib/Controller/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Index extends \MyApp\Controller
+{
+    public function run()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->postMessage();
+        }
+    }
+
+    private function postMessage()
+    {
+        $messageModel = new \MyApp\Model\Message();
+        $messageModel->create([
+        'u_name' => $_POST['u_name'],
+        'content' => $_POST['content']
+      ]);
+
+        header("Location " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+}
